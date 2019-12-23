f05677ce9cf6f5b68409d05434fe6eeb88819e32
ia7ck <23146842+ia7ck@users.noreply.github.com>
2019-12-23 18:01:37 +0900

    use ejs

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const sass = require("gulp-sass");
 const cleancss = require("gulp-clean-css");
 const rename = require("gulp-rename");
 const autoprefixer = require("gulp-autoprefixer");
-const fileinclude = require("gulp-file-include");
 const htmlbeautify = require('gulp-html-beautify');
+const ejs = require("gulp-ejs");
 
 // yarn run gulp watch
 gulp.task("watch", function () {
   gulp.watch("./src/*.scss", gulp.series("build"));
-  gulp.watch("./docs/src/*.html", gulp.series("docs"));
+  gulp.watch("./docs/src/*.ejs", gulp.series("docs"));
 });
 
 gulp.task("build", function (done) {
@@ -30,11 +30,12 @@ gulp.task("build", function (done) {
 // https://www.npmjs.com/package/gulp-file-include
 // https://www.npmjs.com/package/gulp-html-beautify
 gulp.task("docs", function (done) {
-  gulp.src(["./docs/src/index.html"])
-    .pipe(fileinclude())
+  gulp.src(["./docs/src/index.ejs"])
+    .pipe(ejs())
     .pipe(htmlbeautify({
       "indent_size": 2,
     }))
+    .pipe(rename("index.html"))
     .pipe(gulp.dest('./docs'));
   done();
 });
